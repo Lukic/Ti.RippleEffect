cfd05be5b24b5c65572d616876dfa55214ce72b6
Srdjan Lukic-Bardak <srdjan@lukic.nu>
2014-12-22 17:34:04 +0100

    Simple example

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,59 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+// First we need to require our commonJS module.
+var RippleEffect = require("/lib/Ti.RippleEffect");
+ 
+// Yeah we need a window :)
+    var win = Ti.UI.createWindow({
+        backgroundColor:"#000"
+    });
+ 
+    // Lets add some views that will get the RippleEffect on click event.
+    var redView = Ti.UI.createView({
+        width:320,
+        height:150,
+        backgroundColor:"red",
+        top:0,
+        left:0,
+        rippleEffect:true
+    });
+ 
+    var blueView = Ti.UI.createView({
+        width:160,
+        height:150,
+        backgroundColor:"blue",
+        top:150,
+        left:0,
+        rippleEffect:true
+    });
+ 
+    var greenView = Ti.UI.createView({
+        width:160,
+        height:150,
+        backgroundColor:"green",
+        top:150,
+        right:0,
+        rippleEffect:true
+    });
+ 
+    var purpleView = Ti.UI.createView({
+        width:320,
+        height:150,
+        backgroundColor:"purple",
+        top:300,
+        left:0,
+        rippleEffect:true
+    });
+ 
+ 
+    win.add(redView);
+    win.add(blueView);
+    win.add(greenView);
+    win.add(purpleView);
+ 
+    win.addEventListener("click",function(e){
+        if(e.source.rippleEffect){
+// Here we'll pass the clicked object to our animation handler.
+            RippleEffect.create(e);
+        }
+    });
+ 
+    win.open();
